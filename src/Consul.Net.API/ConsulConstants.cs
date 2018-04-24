409ffbaa9c5c1f596f86df1b497b43b229f8d0ed
HelloKitty <andrew.blakely@ymail.com>
2018-04-24 11:56:48 -0500

    Added Consul constants

---
+++ src/Consul.Net.API/ConsulConstants.cs
@@ -0,0 +1,13 @@
+namespace Consul.Net
+{
+	/// <summary>
+	/// Constants used for the Consul.NET API/client.
+	/// </summary>
+	public static class ConsulConstants
+	{
+		/// <summary>
+		/// The globally used user agent for this Consul API/client.
+		/// </summary>
+		public static string UserAgent { get; } = "Consul.Net";
+	}
+}
