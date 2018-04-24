1d49010a038c3d2b0141d66a0013ce38c960536a
HelloKitty <andrew.blakely@ymail.com>
2018-04-24 11:57:01 -0500

    Added User-Agent header for Consul.NET API

---
+++ src/Consul.Net.API/ConsulUserAgentHeaderAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeSafe.Http.Net;
+
+namespace Consul.Net
+{
+	/// <summary>
+	/// <see cref="HeaderAttribute"/> that adds a User-Agent for the Consul client.
+	/// </summary>
+	public sealed class ConsulUserAgentHeaderAttribute : HeaderAttribute
+	{
+		/// <inheritdoc />
+		public ConsulUserAgentHeaderAttribute() 
+			: base("User-Agent", ConsulConstants.UserAgent)
+		{
+
+		}
+	}
+}
